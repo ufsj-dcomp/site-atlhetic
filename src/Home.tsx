4c8b04c188dffe15486e04922c2af86233a92fc4
João Gabriel <jpcardoso0811@gmail.com>
2026-05-09 18:06:50 -0300

    adiciona sidebar responsiva e tela de jogos com busca(AP-9)

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "./sidebar";
+import Sidebar from "./Sidebar";
 
 export default function Home() {
   return (
--- src/Jogos.tsx
+++ src/Jogos.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Sidebar from "./sidebar";
+import Sidebar from "./Sidebar";
 
 export default function Jogos() {
   const [busca, setBusca] = useState("");
